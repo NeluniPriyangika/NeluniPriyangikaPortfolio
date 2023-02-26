55851896c5000808e804386d7066242e63f9deaa
NeluniPriyangika <96222@fhss.sjp.ac.lk>
2023-02-27 00:26:30 +0530

    build the home page of the portfolio and other navigations

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,53 @@
 import React from 'react';
 import './about.css';
+import ME from '../../assets/yellow.png';
+import {FaAward} from 'react-icons/fa';
+import {FiUsers} from 'react-icons/fi';
+import {VscFolderLibrary} from 'react-icons/vsc';
 
 const About = () => {
   return (
-    <div>About</div>
+    <section id='about'>
+      <h5>Get To know</h5>
+      <h2>About Me</h2>
+
+      <div className='container about_container'>
+        <div className='about_me'>
+          <div className='about_me-image'>
+            <img src={ME} alt='About Image'/>
+          </div>
+        </div>
+
+        <div className='about_content'>
+          <div className='about_cards'>
+            <article className='about_card'>
+              <FaAward className='about_icon'/>
+              <h5>Experience</h5>
+              <small>3+ Years Working</small>
+            </article>
+
+            <article className='about_card'>
+              <FiUsers className='about_icon'/>
+              <h5>Clients</h5>
+              <small>200+ Worldwide</small>
+            </article>
+
+            <article className='about_card'>
+              <VscFolderLibrary className='about_icon'/>
+              <h5>Projects</h5>
+              <small>80+ Completed</small>
+            </article>
+          </div>
+
+          <p>
+            ilhsfkasfkasfjasklfjhfffjkljaslkmfioqwfffffffffffffffffffffffffffffffffffffffffffffffffffmlmd
+            knfqqqqqqqqqqqqqqqqqqqqqhjhhhhhlklkhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
+            hguuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuulkk
+            hhhhhhhhhhhhhhhuyuyouyoyoi89uijhkn
+          </p>
+        </div>
+      </div>
+    </section>
   )
 }
 
